1a54f8455e857fee69ef3578eb77b494a68ec13a
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-07-07 14:49:52 +0200

    Bonus 1 fatto

--- index.php
+++ index.php
@@ -3,24 +3,27 @@
         public $title;
         public $director;
         public $releaseYear;
+        public $genres;
 
-        public function __construct($title, $director, $releaseYear){
+        public function __construct($title, $director, $releaseYear, $genres){
             $this->title = $title;
             $this->director = $director;
             $this->releaseYear = $releaseYear;
+            $this->genres = $genres;
         }
-
+        
         public function getInfo(){
-            return "Titolo: " . $this->title . ", Direttore: " . $this->director . ", Anno di pubblicazione: " . $this->releaseYear;
+            // Utilizziamo implode() per unire gli elementi dell'array dei generi in una stringa
+            return "Titolo: " . $this->title . ", Direttore: " . $this->director . ", Anno di pubblicazione: " . $this->releaseYear. ", Generi: " . implode(", ", $this->genres);
         }
-
+        
     }
-
+    
     // Istanzia il primo oggetto Movie
-    $movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
-
+    $movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, ["Mafia", "Giallo", "Drammatico"]);
+    
     // Istanzia il secondo oggetto Movie
-    $movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994);
+    $movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, ["Commedia", "Thriller", "Drammatico"]);
 
     // Stampa le proprietà del primo oggetto Movie
     echo "Proprietà del primo film:<br>";
